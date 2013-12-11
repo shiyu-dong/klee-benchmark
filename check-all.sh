d3dd074ce5527fe8f52fd0efd497a76a79524d8b
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@4e841969-0f3b-58e6-13d5-3710ddd225bc>
2013-12-11 23:10:15 +0000

    adding scripts

---
+++ check-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PROGRAMS="csplit date expand factor fold join tr seq pr mknod"
+PROGRAMS="ex2 ex3 ex8 ex9 ex20 ex30 ex34"
+
+if [ "$1" == "--clean" ]
+then
+  make clean
+  rm -rf klee-* *.gcno *.gcda *.gcov ex? ex?? test_result_new
+else
+  make
+  if [ ! -d "test_result_new" ]; then
+    mkdir test_result_new
+  fi
+  for program in $PROGRAMS
+  do
+    echo $program
+    ./compare-all.sh $program &> test_result_new/result_$program.txt
+  done
+fi
